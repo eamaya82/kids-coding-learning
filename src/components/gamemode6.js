d1c27f952d749124aa5a195e01660ac3968d2780
Edgardo Amaya <32912344+eamaya82@users.noreply.github.com>
2018-10-12 15:26:52 -0500

    gamemode6 audio names
    update file names for audio files

--- src/components/gamemode6.js
+++ src/components/gamemode6.js
@@ -1,35 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import Card from "./cards";
-import Fish from "./fish";
-import Sound_boom from "./sounds/boom.wav";
-import Sound_clap from "./sounds/clap.wav";
-import Sound_kick from "./sounds/kick.wav";
-import Sound_openhat from "./sounds/openhat.wav";
-import Sound_ride from "./sounds/ride.wav";
-import Sound_snare from "./sounds/snare.wav";
-import Sound_tom from "./sounds/tom.wav";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-import Animal_cow from "./sounds/animal_cow.wav";
-import Animal_elephant from "./sounds/animal_elephant.wav";
-import Animal_frog from "./sounds/animal_frog.wav";
-import Animal_kitty from "./sounds/animal_kitty.wav";
-import Animal_owl from "./sounds/animal_owl.wav";
-import Animal_rooster from "./sounds/animal_rooster.wav";
-import Animal_sheep from "./sounds/animal_sheep.wav";
+import animal_cow from './sounds/animal_cow.wav';
+import animal_elephant from './sounds/animal_elephant.wav';
+import animal_frog from './sounds/animal_frog.wav';
+import animal_kitty from './sounds/animal_kitty.wav';
+import animal_owl from './sounds/animal_owl.wav';
+import animal_rooster from './sounds/animal_rooster.wav';
+import animal_sheep from './sounds/animal_sheep.wav';
 
-import Piano_a from "./sounds/piano_a.wav";
-import Piano_b from "./sounds/piano_b.wav";
-import Piano_c from "./sounds/piano_c.wav";
-import Piano_d from "./sounds/piano_d.wav";
-import Piano_e from "./sounds/piano_e.wav";
-import Piano_f from "./sounds/piano_f.wav";
-import Piano_g from "./sounds/piano_g.wav";
+import sound_tom from './sounds/sound_tom.wav';
+import sound_clap from './sounds/sound_clap.wav';
+import sound_kick from './sounds/sound_kick.wav';
+import sound_openhat from './sounds/sound_openhat.wav';
+import sound_boom from './sounds/sound_boom.wav';
+import sound_ride from './sounds/sound_ride.wav';
+import sound_snare from './sounds/sound_snare.wav';
 
-import "../App.css";
+import piano_a from './sounds/piano_a.wav';
+import piano_b from './sounds/piano_b.wav';
+import piano_c from './sounds/piano_c.wav';
+import piano_d from './sounds/piano_d.wav';
+import piano_e from './sounds/piano_e.wav';
+import piano_f from './sounds/piano_f.wav';
+import piano_g from './sounds/piano_g.wav';
+
+import Card from './cards';
+import Fish from './fish';
+
+import '../App.css';
 
 library.add(faArrowLeft);
 
@@ -41,29 +43,29 @@ class Game extends Component {
     this.state = {
       cardList: [],
       cardDrop: [
-        { icon: "question", color: 6, id: 8, soundsrc: Animal_cow },
-        { icon: "question", color: 7, id: 9, soundsrc: Animal_elephant },
-        { icon: "question", color: 8, id: 10, soundsrc: Animal_frog },
-        { icon: "question", color: 9, id: 11, soundsrc: Animal_kitty },
-        { icon: "question", color: 10, id: 12, soundsrc: Animal_owl },
-        { icon: "question", color: 11, id: 13, soundsrc: Animal_rooster },
-        { icon: "question", color: 12, id: 14, soundsrc: Animal_sheep },
+        { icon: 'question', color: 6, id: 8, src: animal_cow },
+        { icon: 'question', color: 7, id: 9, src: animal_elephant },
+        { icon: 'question', color: 8, id: 10, src: animal_frog },
+        { icon: 'question', color: 9, id: 11, src: animal_kitty },
+        { icon: 'question', color: 10, id: 12, src: animal_owl },
+        { icon: 'question', color: 11, id: 13, src: animal_rooster },
+        { icon: 'question', color: 12, id: 14, src: animal_sheep },
 
-        { icon: "drum", color: 0, id: 1, soundsrc: Sound_tom },
-        { icon: "drum", color: 1, id: 2, soundsrc: Sound_clap },
-        { icon: "drum", color: 2, id: 3, soundsrc: Sound_kick },
-        { icon: "drum", color: 3, id: 4, soundsrc: Sound_openhat },
-        { icon: "drum", color: 4, id: 5, soundsrc: Sound_boom },
-        { icon: "drum", color: 5, id: 6, soundsrc: Sound_ride },
-        { icon: "drum", color: 6, id: 7, soundsrc: Sound_snare },
+        { icon: 'drum', color: 0, id: 1, src: sound_tom },
+        { icon: 'drum', color: 1, id: 2, src: sound_clap },
+        { icon: 'drum', color: 2, id: 3, src: sound_kick },
+        { icon: 'drum', color: 3, id: 4, src: sound_openhat },
+        { icon: 'drum', color: 4, id: 5, src: sound_boom },
+        { icon: 'drum', color: 5, id: 6, src: sound_ride },
+        { icon: 'drum', color: 6, id: 7, src: sound_snare },
 
-        { icon: "music", color: 11, id: 15, soundsrc: Piano_a },
-        { icon: "music", color: 11, id: 16, soundsrc: Piano_b },
-        { icon: "music", color: 11, id: 17, soundsrc: Piano_c },
-        { icon: "music", color: 11, id: 18, soundsrc: Piano_d },
-        { icon: "music", color: 11, id: 19, soundsrc: Piano_e },
-        { icon: "music", color: 11, id: 20, soundsrc: Piano_f },
-        { icon: "music", color: 11, id: 21, soundsrc: Piano_g }
+        { icon: 'music', color: 11, id: 15, src: piano_a },
+        { icon: 'music', color: 11, id: 16, src: piano_b },
+        { icon: 'music', color: 11, id: 17, src: piano_c },
+        { icon: 'music', color: 11, id: 18, src: piano_d },
+        { icon: 'music', color: 11, id: 19, src: piano_e },
+        { icon: 'music', color: 11, id: 20, src: piano_f },
+        { icon: 'music', color: 11, id: 21, src: piano_g }
       ],
       cardColor: 0,
       currentCard: null,
@@ -95,7 +97,7 @@ class Game extends Component {
     return newcolor;
   }
   playsound(ev, id) {
-    let source = document.getElementById("audio-" + id);
+    let source = document.getElementById('audio-' + id);
     source.currentTime = 0;
     source.play();
     this.commandfish();
@@ -140,15 +142,13 @@ class Game extends Component {
           id={drop.id}
           className="card cardmenu"
           style={{
-            top: Math.floor(index / 7) * 30 + 5 + "%",
-            left: `${(index % 7) * 13 + 7}%`
+            top: Math.floor(index / 7) * 30 + 5 + '%',
+            left: `${(index % 7) * 12 + 9}%`
           }}
           onClick={e => this.playsound(e, drop.id)}
         >
-          <audio id={"audio-" + drop.id}>
-            <source src={drop.soundsrc} />
-          </audio>
-          <Card icon={[drop.icon]} color={drop.color} size={4} />
+          <audio id={'audio-' + drop.id} type="audio/wav" src={drop.src} />
+          <Card icon={[drop.icon]} color={drop.color} size={3} />
         </div>
       );
     });
